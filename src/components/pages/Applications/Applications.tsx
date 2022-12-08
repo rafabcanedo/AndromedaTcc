472950f0e49b037d7f4aca4f2733e65ec8f4d99c
rafabcanedo <rafaitecanedo@gmail.com>
2022-12-08 12:42:45 -0300

    finish sem gmail

--- src/components/pages/Applications/Applications.tsx
+++ src/components/pages/Applications/Applications.tsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 
 function Applications() {
  return(
-    <>
-    <div className="applications-title">
+  <>
+  <div className="applications-title">
     <h1>Aplicações</h1>
     <h3>
       Inicie aqui sua trilha da produtividade em Andrômeda <br />
@@ -13,73 +13,70 @@ function Applications() {
       A viagem ja se inicia ao "universo" da produtividade.
     </h3>
     </div>
-
-    {/*<div className='pricing container section'>
-    <h1 className='section__title'>3 Aplicaçoes Disponíveis</h1>*/}
-     <div className="body-applications">
-     <header className="header-title">
-       3 Aplicações disponíveis
-     </header>
-     </div>
-
-     <div className="container-applications">
-      
-      <div className="applications-list">
-       <h3>
-        Pomodoro
-       </h3>
-       <span>
-         Pomodoro é uma ferramenta foi desenvolvida para se trabalhar <br />
+  <br />
+  <br />
+  <br />
+  <br />
+  <div className="container-home">
+  <div className="ctn-apps">
+    
+    <div className="box-apps-home">
+    <div className="ctn-photo-apps">
+    </div>
+    <h2 className="title-img-home">
+     Pomodoro
+    </h2>
+    <br />
+    <p className="p-img-home">
+    Pomodoro é uma ferramenta foi desenvolvida para se trabalhar <br />
          junto ao tempo, com um cronômetro flexiível você poderá definir <br />
          seu próprio tempo para suas atividades listadas.
-       </span>
-      <Link to='/pomodoro'>
-      <button className="btn-applications">
-       Pomodoro
-      </button>
-      </Link>
-      </div>
- 
+    </p>
+    <div className='button-app-margin'>
+    <Link to="/pomodoro">
+    <button className="btn-applications">Pomodoro</button>
+    </Link>
+    </div>
+    </div>
 
-      <div className="applications-list">
-       <h3>
-        Lista de Tarefas
-       </h3>
-       <span>
-         Lista de Tarefas(ToDo List em inglês) é uma ferramenta usada <br />
+    <div className="box-apps-home">
+    <h2 className="title-img-home">
+     Lista de Tarefas
+    </h2>
+    <br />
+    <p className="p-img-home">
+    Lista de Tarefas(ToDo List em inglês) é uma ferramenta usada <br />
          para listar suas atividades diárias, semanais ou atividades <br />
          de um projeto específico sendo por ordem de prioridade ou <br />
          por ordem por linha de tempo.
-       </span>
-       <Link to='/todolist'>
-       <button className="btn-applications"> 
-        ToDo List 
-       </button>
-       </Link>
-      </div>
-
-
-      <div className="applications-list">
-       <h3>
-        Calendário
-       </h3>
-       <span>
-         Calendário é uma ferramenta padrão, já utilizada há décadas <br />
+    </p>
+    <div className='button-app-margin-2'>
+    <Link to="/todolist">
+    <button className="btn-applications">Lista de Tarefas</button>
+    </Link>
+    </div>
+    </div>
+    
+    <div className="box-apps-home">
+    <h2 className="title-img-home">
+     Calendario
+    </h2>
+    <br />
+    <p className="p-img-home">
+    Calendário é uma ferramenta padrão, já utilizada há décadas <br />
          e muito prática. Aqui você pode se organizar seus compromissos <br />
          conforme o dia, semana e mês de sua preferência.
-       </span>
-       <Link to='/calendario'>
-      <button className="btn-applications"> 
-        Calendar 
-      </button>
-      </Link>
-      </div>
-      </div>
-    
-    </>
+    </p>
+    <div className='button-app-margin'>
+    <Link to="/calendario">
+    <button className="btn-applications">Calendario</button>
+    </Link>
+    </div>
+    </div>
+    </div>
+    </div>
+  </>
  );
 }
 
 export default Applications;
-
-// <ArrowDown size={32} color="#633BBC" size={28} />
